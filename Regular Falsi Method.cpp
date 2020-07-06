dc3ecdefe257f27e4fbcc21bee3f2d80fb7f26d1
zaibidev <muhammadzohaibarshad18@gmail.com>
2020-07-06 20:24:48 +0500

    I is an algorithmic C++ code of Regular Falsi Method used in numerical computing methods (theoratical and algorithmic course)

---
+++ Regular Falsi Method.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+float polynomial_expression(float p)
+{
+	return (p*p - 3);
+}
+int main()
+{
+	float m = NULL, n = NULL, ans = NULL;
+	int i = NULL;
+	for (i = 0; i < 100; i++)
+	{
+		if (polynomial_expression(i) < 0 && polynomial_expression(i + 1) > 0)
+		{
+			cout << "The root is : = [ " << i << " ,  " << i + 1 << " ] " << endl;
+			cout << "As  f(" << i << ")" << " =  " << polynomial_expression(i) << " > 0" << endl;
+			cout << "And f(" << i + 1 << ")" << "  = " << polynomial_expression(i + 1) << " < 0" << endl;
+			m = i;
+			n = i + 1;
+			break;
+		}
+		if (polynomial_expression(i) > 0 && polynomial_expression(i + 1) < 0)
+		{
+			cout << "The root is : = [ " << i << " ,  " << i + 1 << " ] " << endl;
+			cout << "As  f(" << i << ")" << " =  " << polynomial_expression(i) << " < 0" << endl;
+			cout << "And f(" << i + 1 << ")" << "  = " << polynomial_expression(i + 1) << " > 0" << endl;
+			m = i;
+			n = i + 1;
+			break;
+		}
+	}
+	i = 1;
+
+	while (i < 100)
+	{
+		ans = (m + n) / 2;
+		cout << "For next approximation " << endl << i << " The number of iteration is " << m << " and " << n << endl;
+		ans = (m*polynomial_expression(n) - n * polynomial_expression(m)) / (polynomial_expression(n) - polynomial_expression(m));
+		cout << "f(" << ans << ")" << " = " << polynomial_expression(ans) << endl << endl;
+		if (polynomial_expression(ans) > 0)
+		{
+			n = ans;
+		}
+		else if (polynomial_expression(ans) < 0)
+		{
+			m = ans;
+		}
+		else
+			break;
+		i = i + 1;
+	}
+	cout << "Approximated root is : = " << ans << endl;
+	system("pause");
+}
